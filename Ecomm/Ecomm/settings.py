b1a323c55c6f24b8c879edd5551dc5560d9a5ec6
nkdkd <nkdkd0144@gmail.com>
2024-08-07 14:36:30 +0530

    allow hosts

--- Ecomm/Ecomm/settings.py
+++ Ecomm/Ecomm/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-x4m$gfeda-r+)u05g*bzm%8#_vz&8-wl^3epo45gqi#_eqwvtq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','16.171.199.1' 'djangotest.com', 'django-ecommerce-production-81b6.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://djangotest.com', 'https://django-ecommerce-production-81b6.up.railway.app']
 
